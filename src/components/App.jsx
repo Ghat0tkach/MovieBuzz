541d6d38d95fad9bdb270f48e6f5b5b54c4b4780
Vikramaditya <awesomevikram3@gmail.com>
2023-07-28 18:42:39 +0530

    feat:added first files
    Signed-off-by: Vikramaditya <awesomevikram3@gmail.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { lazy } from 'react'
+import { CssBaseline } from '@mui/material'
+import { BrowserRouter, Route,Switch ,Routes} from 'react-router-dom'
+import useStyles from './styles.js'
+// import {Actors, MovieInformation,Movies,Profile,Navbar} from './index.js'
+import Actors from './Actors/Actors';
+import Movies from './Movies/Movies';
+import MovieInformation from './MovieInformation/MoviesInformation';
+import Profile from './Profile/Profile';
+import Navbar from './Navbar/Navbar';
 
-export const App = () => {
+
+export default function App() {
+  const classes=useStyles();
+
+
   return (
-    <div>App</div>
+    <div className={classes.root}>
+        <CssBaseline/>
+        <Navbar/>
+        <main className={classes.content}>
+          <div className={classes.toolbar}>
+          <BrowserRouter>
+           <Routes>
+              <Route index element={<Actors/>} />
+              <Route path="movies" element={<Movies/>} />
+              <Route path="movieInformation" element={<MovieInformation/>} />
+              <Route path="actors" element={<Actors/>} />
+              <Route path="profile" element={<Profile/>} />
+              </Routes>
+        </BrowserRouter>   
+       
+          </div>
+        </main>
+        
+    </div>
   )
 }
